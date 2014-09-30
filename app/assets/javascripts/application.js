9eda196796a4cd17517e05cf81b6a9a1bb424c86
Marc <marc.bluemner@gmail.com>
2014-09-30 16:52:53 +0200

    changed application.js to use angular libs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require angular
+//= require angular-animate
+//= require angular-ui-router
+//= require angular-rails-templates
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
